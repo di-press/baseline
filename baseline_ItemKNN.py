e9f6afb261973930f019639c3f76fea2d01e3aa6
di-press <diany_press@usp.br>
2021-08-26 19:50:17 -0300

    finish ItemKNN with hamming up to 50 neighbors

--- baseline_ItemKNN.py
+++ baseline_ItemKNN.py
@@ -36,7 +36,36 @@ def item_knn_rating_prediction_hamming():
                     write_predictions = True).compute()
 
 
+def item_knn_experiments(similarity, num_neighbors):
+
+    #complete database file, without splitting
+    input_file = Path.cwd().joinpath('baseline_df.csv') 
+    input_file = str(input_file)
+
+    prediction_dir = Path.cwd().joinpath('knn_rating_prediction')
+    prediction_dir  = str(prediction_dir)
+
+    recommender = ItemKNN(similarity_metric= similarity, k_neighbors= num_neighbors)
+
+    CrossValidation(input_file, recommender, prediction_dir, k_folds = 10, 
+                    header=1, recommender_verbose=False, as_table=False,
+                    evaluation_in_fold_verbose=False,del_folds=True,
+                    write_predictions = False).compute()
+
+
+def generate_up_to_50_neighbors_RMSE(similarity):
+
+    #neighbors from 1 to 50:
+    for num_neighbors in range(1, 51):
+
+        print("k= ", num_neighbors)
+        item_knn_experiments(similarity, num_neighbors)
+
 if __name__ == '__main__':
 
-    item_knn_rating_prediction_cosine()
+    #item_knn_rating_prediction_cosine()
     #item_knn_rating_prediction_hamming()
+
+    #generate_up_to_50_neighbors_RMSE("cosine")
+
+    generate_up_to_50_neighbors_RMSE("hamming")
