ae4b3ed21f7c11960216cef9fc6788f42b74b4b0
di-press <diany_press@usp.br>
2021-08-27 23:52:04 -0300

    done some baseline experiments

--- baseline_ItemKNN.py
+++ baseline_ItemKNN.py
@@ -68,4 +68,12 @@ if __name__ == '__main__':
 
     #generate_up_to_50_neighbors_RMSE("cosine")
 
-    generate_up_to_50_neighbors_RMSE("hamming")
+    #generate_up_to_50_neighbors_RMSE("hamming")
+
+    generate_up_to_50_neighbors_RMSE("minkowski")
+
+    #generate_up_to_50_neighbors_RMSE("chebyshev")
+
+    #generate_up_to_50_neighbors_RMSE("manhattan")
+
+    #generate_up_to_50_neighbors_RMSE("mahalanobis")
--- experiments.py
+++ experiments.py
@@ -38,6 +38,80 @@ def exp_baseline_4():
 
     graphics.MRSE_graphic('4', 50, MRSE_values_list, "ItemKNN", 10, "hamming","deeppink")
 
+# UserKNN minkowski
+def exp_baseline_5():
+
+    file = Path.cwd().joinpath('UserKNN_minkowski_1_to_50_neighbors.txt')
+    MRSE_values_list = graphics.MRSE_values_from_file(file)
+    #print(MRSE_values_list)
+
+    graphics.MRSE_graphic('5', 50, MRSE_values_list, "UserKNN", 10, "minkowski","deeppink")
+
+# UserKNN chebyshev
+def exp_baseline_6():
+
+    file = Path.cwd().joinpath('UserKNN_chebyshev_1_to_50_neighbors.txt')
+    MRSE_values_list = graphics.MRSE_values_from_file(file)
+    #print(MRSE_values_list)
+
+    graphics.MRSE_graphic('6', 50, MRSE_values_list, "UserKNN", 10, "chebyshev","chartreuse")
+
+# UserKNN manhattan
+def exp_baseline_7():
+
+    file = Path.cwd().joinpath('UserKNN_manhattan_1_to_50_neighbors.txt')
+    MRSE_values_list = graphics.MRSE_values_from_file(file)
+    #print(MRSE_values_list)
+
+    graphics.MRSE_graphic('7', 50, MRSE_values_list, "UserKNN", 10, "manhattan","indigo")
+
+# UserKNN mahalanobis
+def exp_baseline_8():
+
+    file = Path.cwd().joinpath('UserKNN_mahalanobis_1_to_50_neighbors.txt')
+    MRSE_values_list = graphics.MRSE_values_from_file(file)
+    #print(MRSE_values_list)
+
+    graphics.MRSE_graphic('8', 50, MRSE_values_list, "UserKNN", 10, "mahalanobis","mediumvioletred")
+
+# ItemKNN minkowski
+def exp_baseline_9():
+
+    file = Path.cwd().joinpath('ItemKNN_minkowski_1_to_50_neighbors.txt')
+    MRSE_values_list = graphics.MRSE_values_from_file(file)
+    #print(MRSE_values_list)
+
+    graphics.MRSE_graphic('9', 50, MRSE_values_list, "ItemKNN", 10, "minkowski","steelblue")
+
+
+# ItemKNN chebyshev
+def exp_baseline_10():
+
+    file = Path.cwd().joinpath('ItemKNN_chebyshev_1_to_50_neighbors.txt')
+    MRSE_values_list = graphics.MRSE_values_from_file(file)
+    #print(MRSE_values_list)
+
+    graphics.MRSE_graphic('10', 50, MRSE_values_list, "ItemKNN", 10, "chebyshev","palegreen")
+
+# ItemKNN manhattan
+def exp_baseline_10():
+
+    file = Path.cwd().joinpath('ItemKNN_manhattan_1_to_50_neighbors.txt')
+    MRSE_values_list = graphics.MRSE_values_from_file(file)
+    #print(MRSE_values_list)
+
+    graphics.MRSE_graphic('10', 50, MRSE_values_list, "ItemKNN", 10, "manhattan","bisque")
+
+# ItemKNN mahalanobis
+def exp_baseline_11():
+
+    file = Path.cwd().joinpath('ItemKNN_mahalanobis_1_to_50_neighbors.txt')
+    MRSE_values_list = graphics.MRSE_values_from_file(file)
+    #print(MRSE_values_list)
+
+    graphics.MRSE_graphic('11', 50, MRSE_values_list, "ItemKNN", 10, "mahalanobis","plum")
+
+
 if __name__ == '__main__':
 
     #exp_baseline_1()
@@ -46,4 +120,4 @@ if __name__ == '__main__':
 
     #exp_baseline_3()
 
-    exp_baseline_4()
+    #exp_baseline_4()
