a80ccb64f7369ac1718b199341840d555447298c
di-press <diany_press@usp.br>
2021-08-28 16:53:07 -0300

    finish ItemKNN manhattan

--- baseline_ItemKNN.py
+++ baseline_ItemKNN.py
@@ -72,8 +72,8 @@ if __name__ == '__main__':
 
     #generate_up_to_50_neighbors_RMSE("minkowski")
 
-    generate_up_to_50_neighbors_RMSE("chebyshev")
+    #generate_up_to_50_neighbors_RMSE("chebyshev")
 
-    #generate_up_to_50_neighbors_RMSE("manhattan")
+    generate_up_to_50_neighbors_RMSE("cityblock")
 
     #generate_up_to_50_neighbors_RMSE("jaccard")
